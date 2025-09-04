5542260ba076f3635f400b70a7adc5d3fea100c0
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2025-09-04 14:26:22 -0600

    integrated first add task phase

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import cli
+from tarea import Tarea
 
 def main():
     parser = cli.start_parser()
     args = parser.parse_args()
     print(args)
     
-    pass
+    if args.command == "add":
+        tarea = Tarea(args.title, args.priority, args.date, args.description, args.completed)
+        tarea.saveToJson()
+        print("Task added successfully.")
 
 if __name__ == "__main__":
     main()
