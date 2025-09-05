d87939f81bdce219f51bd2fe7b0e04cf4f00c3fc
Manuel Sandoval Arroyo <manusandoval05@gmail.com>
2025-09-05 09:31:54 -0600

    add function definitions for repo

--- repo.py
+++ repo.py
@@ -2,22 +2,34 @@ from tarea import Tarea
 import io_json
 
 def get_task(id: int) -> Tarea:
-    pass
+    io_json.get_task_dict_from_json(id)
+    task = Tarea.from_dict(task) if task else None
+    return task
 
 def get_task_list() -> list[Tarea]:
-    pass
+    task_dictionary = io_json.get_array_from_json()
+    return [Tarea.from_dict(task) for task in task_dictionary]
 
 def add_task(tarea: Tarea) -> Tarea:
-    pass
+    io_json.add_to_json(tarea.dic())
+    return tarea
 
-def find_tasks(searchTerm: str) -> list[Tarea]:
-    pass
+def find_tasks(search_term: str) -> list[Tarea]:
+    all_tasks = get_task_list()
+    matching_tasks = [task for task in all_tasks if search_term.lower() in task.titulo.lower() or search_term.lower() in task.descripcion.lower()]
+    return matching_tasks
 
 def delete_task(id: int) -> Tarea:
-    pass
+    task = get_task(id)
+    io_json.delete_task_from_json(id)
+    return task
 
 def save_task():
     pass
 
 def complete_task(id: int) -> Tarea:
-    pass
+    task = get_task(id)
+    if task:
+        task.complete()
+        io_json.add_to_json(task.dic())
+    return task
