b28fd708b1be28110bace7f0cac502c27345f44c
thrinkler <aeshadow0935@gmail.com>
2025-10-10 17:56:12 -0600

    Finish first draft

--- app/export_html.py
+++ app/export_html.py
@@ -2,23 +2,44 @@ from . import repo
 
 tareas = repo.get_task_list()
 
-tareas_html = ""
+tareas_html = """<tr>
+    <th>ID</th>
+    <th>Titulo</th>
+    <th>Prioridad</th>
+    <th>Fecha</th>
+    <th>Descripción</th>
+    <th>Tags</th>
+    <th>Completada</th>
+  </tr>"""
+
+
 for tarea in tareas:
-    tareas_html += f'<li class="Tarea">{tarea.titulo}</li>\n'
+    
+    tareas_html += f'<tr class="{"completada" if tarea.completada else ""}">\
+    <td>{tarea.id}</td>\
+    <td>{tarea.titulo}</td>\
+    <td>{tarea.prioridad}</td>\
+    <td>{tarea.fecha}</td>\
+    <td>{tarea.descripcion}</td>\
+    <td>{", ".join(tarea.tags) if tarea.tags != None else ""}</td>\
+    <td>{"X" if tarea.completada else ""}</td>\
+    </tr>\n'
+
+
 
 html_completo = f"""
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Mi Agenda</title>
+    <title>Agenda TikiTiki</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h1>Mi Lista de Tareas</h1>
-    <ul>
+    <h1>La Agenda TikiTiki</h1>
+    <table>
         {tareas_html}
-    </ul>
+    </table>
 </body>
 </html>
 """
